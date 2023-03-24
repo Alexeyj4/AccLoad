c07e12d4dbe996bd6901a38c2ac21e087f62ce56
Vlad <Klisunov.vladdi94@gmail.com>
2023-03-24 17:43:40 +0300

    ...

--- AccLoad/AccLoad/AccLoad.ino
+++ AccLoad/AccLoad/AccLoad.ino
@@ -1,9 +1,42 @@
+int numOfSlots=3;
+char* nam[]={"Ячейка1","Ячейка2","Ячейка3"};
+const int Upin[]={14,16,18};
+const int Ipin[]={15,17,19};
+
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  for(int i=0;i<numOfSlots;i++)  
+  {
+    pinMode(Upin[i],INPUT);
+    pinMode(Ipin[i],INPUT);
+  }
+
+analogReference(INTERNAL);
+}
+
+
+float toVolts(int Umeas)
+{
+  return Umeas/32.7;
+}
+
+float toAmperes(int Imeas)
+{
+  return Imeas/229.1;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  for(int i=0;i<numOfSlots;i++)
+  {
+    int Umeas=analogRead(Upin[i]);
+    int Imeas=analogRead(Ipin[i]);
+    Serial.println(nam[i]);
+    Serial.println(toVolts(Umeas));
+    Serial.println(toAmperes(Imeas));
+  }
+  delay(1000);
 }
